e506a15840db4e4423c53dc23aa41775885bc805
SofiaJacques <sofia_jacques@hotmail.com>
2021-03-11 00:38:21 +0900

    Update frame2video.py
    1. Fixed filesystem based frame iteration
    2. Change console print to logging

--- scripts/frame2video.py
+++ scripts/frame2video.py
@@ -3,22 +3,34 @@ import numpy as np
 import glob
 import os
 import time
-from progress.bar import IncrementalBar
+#from progress.bar import IncrementalBar
+import logging
 
-folderIn = "/mnt/disks/disk-1/jester_dataset/dataset/20bn-jester-v1/"
-folderOut = "/home/gestures_aidl/aidl/videos/"
+
+folderIn = "/mnt/disks/jester_dataset/dataset/20bn-jester-v1/"
+folderOut = "/home/gestures_aidl/videos/"
 
 if not os.path.exists(folderOut):
     os.makedirs(folderOut)
 
 directories = [f for f in  os.scandir(folderIn) if f.is_dir() ]
-bar = IncrementalBar('Completed', max = len(directories),  suffix='%(index)d/%(max)d - %(percent).1f%% - ETA: %(eta)ds ')
+#bar = IncrementalBar('Completed', max = len(directories),  suffix='%(index)d/%(max)d - %(percent).1f%% - ETA: %(eta)ds ')
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-for directory in directories:
-    #print("Processing images from " + directory.name)
+## Logging variables ##
+log_int = 50
+logging.basicConfig(filename='/home/gestures_aidl/scripts/logs/frame2video.log')
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
+vid_s_time = time.time()
+## Logging variables ##
+
+for num, directory in enumerate(directories):
     img_array = []
-    for filename in glob.glob(directory.path+'/*.jpg'):
+    #If file has already been processed in a previous run, ignore.
+    if os.path.exists(folderOut + directory.name+'.mp4'):
+        continue
+    for filename in sorted(glob.glob(directory.path+'/*.jpg')):
         img = cv2.imread(filename)
         img_array.append(img)
 
@@ -29,6 +41,12 @@ for directory in directories:
     for i in range(len(img_array)):
         out.write(img_array[i])
     out.release()
-    #time.sleep(0.2)
-    bar.next()
-bar.finish()
+    if num > 0 and num % log_int == 0:
+        vid_e_time = time.time()
+        loop_t = vid_e_time - vid_s_time
+        vid_s_time = time.time()
+        t = (len(directories) - num)/log_int * loop_t /3600
+        logger.info('%04d/%04d is done (%4.2f%%). Time for processing %02d videos: %4.2f seconds. ETA: %4.2fh' % (num, len(directories),num/len(directories)*100,log_int, loop_t, t))
+
+    #bar.next()
+#bar.finish()
